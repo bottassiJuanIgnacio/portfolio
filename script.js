685b49bb7d7f0d33dd2993a7dbe66e8c9baf5baa
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-12-13 03:16:09 -0300

    Modifico textos y colores

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ function efectoHabilidades() {
     if (distancia_skills >= 300) {
         document.getElementById("html").classList.add("barra-progreso1");
         document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
+        document.getElementById("react").classList.add("barra-progreso3");
+        document.getElementById("bd").classList.add("barra-progreso4");
     }
 
 }
