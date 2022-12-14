d764196878bf1d9a2fb7cb8ac40fa3e0838a486e
bottassiJuanIgnacio <bottassi2012@gmail.com>
2022-12-13 23:01:29 -0300

    docs, servicios

--- php/form.php
+++ php/form.php
@@ -17,9 +17,9 @@
     $header = 'Este mail fue enviado desde el portfolio web';
     $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
 
-    mail($email, $asunto, $mensajeCompleto, $header);
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
 
     echo "<script> alert("correo enviado exitosamente")</script>";
-    echo "<script> setTimeout(\"location.href='exito.html'\",5000)</script>";
+    echo "<script> setTimeout(\"location.href='../index.html'\",5000)</script>";
 
 ?>
